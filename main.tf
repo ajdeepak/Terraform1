1551d0876041de6455f3e56d62b3dba84d96d7e6
deepak verma <53348739+ajdeepak@users.noreply.github.com>
2025-12-03 12:45:26 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,11 @@ module "vpc" {
 
 module "compute" {
     source = "./modules/compute"
-    public_subnets = module.vpc.public_subnet
+    public_subnets = module.vpc.public_subnets
     vpc_id        = module.vpc.vpc_id
     instance_type = var.instance_type
     ami_id = var.ami_id
 }
+
 
 
