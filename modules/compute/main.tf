35506115fb193fcf6fb52eb0f2006500f44cdff5
deepak verma <53348739+ajdeepak@users.noreply.github.com>
2025-12-03 10:19:02 -0500

    Update main.tf

---
+++ modules/compute/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  count = 2
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  subnet_id = module.vpc.public_subnet[count.index]
+  associate_public_ip_address = true
+
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name = "web-${count.index + 1}"
+  }
+}
